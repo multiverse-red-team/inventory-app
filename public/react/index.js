9056d63cb29ac1053e709169c29f0ae954dc7c5c
stevenjlance <steven.j.lance@gmail.com>
2022-09-28 10:19:50 -0400

    Updated createRoot

--- public/react/index.js
+++ public/react/index.js
@@ -1,10 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import { createRoot } from 'react-dom/client';
 import 'regenerator-runtime/runtime'
 
 import {App} from './components/App';
 
-ReactDOM.render(
-	<App />,
-	document.getElementById('root')
-)
+const root = createRoot(document.getElementById("root"));
+root.render(<App />);
