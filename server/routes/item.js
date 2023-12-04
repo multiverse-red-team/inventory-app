d7c5485c4955553b83edea4b64904c2ceef7c433
Andrew <106277595+andrewozo@users.noreply.github.com>
2023-12-04 12:55:10 -0500

    single item router (#5)

--- server/routes/item.js
+++ server/routes/item.js
@@ -1,11 +1,21 @@
 const express = require("express");
-const {Item} = require("../models/index")
+const { Item } = require("../models/index");
 const router = express.Router();
 
 // Get All Items
-router.get("/", async(req,res) =>{
-    const items = await Item.findAll();
-    res.json(items)
-})
+router.get("/", async (req, res) => {
+  const items = await Item.findAll();
+  res.json(items);
+});
+
+router.get("/:id", async (req, res, next) => {
+  try {
+    const item = await Item.findByPk(req.params.id);
+
+    res.json(item);
+  } catch (error) {
+    next(error);
+  }
+});
 
 module.exports = router;
