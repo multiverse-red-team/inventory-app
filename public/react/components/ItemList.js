df23011d6e65827f8f5303094adad612acb3339d
crystalchavez99 <chavezcrystal1999@yahoo.com>
2023-12-04 14:06:56 -0800

    react route implemented, get all items works just need to apply a route for the single item

--- public/react/components/ItemList.js
+++ public/react/components/ItemList.js
@@ -1,11 +1,32 @@
 import React, { useState, useEffect } from "react";
 import Item from "./Item";
+import apiURL from "../api";
+import { Link } from "react-router-dom";
 
-function ItemList({ items, oneItem }) {
+function ItemList() {
+
+  const [items, setItems] = useState([]);
+
+  async function fetchItems() {
+    try {
+      const response = await fetch(`${apiURL}/items`);
+      const itemData = await response.json();
+      setItems(itemData);
+    } catch (err) {
+      console.log("Oh no an error! ", err);
+    }
+  }
+
+  useEffect(() => {
+    fetchItems();
+  }, []);
+
   return (
     <div>
+      <h1>Sauce Store</h1>
+      <h2>All Items</h2>
       {items.map((item) => (
-        <Item key={item.id} item={item} onClick={oneItem} />
+        <Link to={`/items/${item.id}`}><Item key={item.id} item={item}/></Link>
       ))}
     </div>
   );
