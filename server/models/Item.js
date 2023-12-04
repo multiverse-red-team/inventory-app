9e2c7f95ee6481513d3e05bbeaadff8e0247e955
Kevin Dutzy <kddutz15@gmail.com>
2023-12-04 10:30:56 -0700

    created item model

---
+++ server/models/Item.js
@@ -0,0 +1,16 @@
+const {Sequelize} = require('sequelize')
+const {sequelize} = require('../db')
+
+
+const Item = sequelize.define("items", {
+  name: Sequelize.STRING,
+  description: Sequelize.STRING,
+  price: Sequelize.INTEGER,
+  category: Sequelize.STRING,
+  image: Sequelize.STRING,
+});
+
+module.exports = {
+  db: sequelize,
+  Item,
+};
