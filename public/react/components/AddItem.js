58e260dd2cf51a9af43ef510500c03fa8a630726
Andrew <106277595+andrewozo@users.noreply.github.com>
2023-12-07 16:24:29 -0500

    Andrew o (#46)
    * fixed styling
    
    * changed npm start back to original
    
    * refactored addItem code
    
    * fixes

--- public/react/components/AddItem.js
+++ public/react/components/AddItem.js
@@ -76,7 +76,6 @@ function AddItem() {
           <input
             name="category"
             className="form-input"
-            name="category"
             type="text"
             placeholder="Item category"
             required={true}
